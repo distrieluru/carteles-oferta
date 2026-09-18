f88344d8fd39c576635891c2fc5f8b9c51601fd0
distrieluru <rubenmenedez2025@gmail.com>
2026-09-17 21:23:39 -0300

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,27 @@
+var CACHE = "carteles-v1";
+var ARCHIVOS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ARCHIVOS); }).catch(function(){}));
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(ks){
+    return Promise.all(ks.map(function(k){ if(k!==CACHE) return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e){
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      return hit || fetch(e.request).then(function(res){
+        var copia = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, copia); }).catch(function(){});
+        return res;
+      }).catch(function(){ return caches.match("./index.html"); });
+    })
+  );
+});
